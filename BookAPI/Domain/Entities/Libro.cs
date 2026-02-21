ca04439d51e6ac6d25d22dd44d818b642a1140c3
Waldo Feliz <36774520+waldofeliz@users.noreply.github.com>
2026-02-21 09:36:10 -0400

    Delete BookAPI directory

--- BookAPI/Domain/Entities/Libro.cs
+++
@@ -1,9 +0,0 @@
-namespace Domain.Entities;
-
-public class Libro
-{
-    public int Id { get; set; }
-    public string Titulo { get; set; }
-    public string? Descripcion { get; set; } = String.Empty;
-    public DateTime FechaPublicacion { get; set; } = DateTime.MinValue;
-}
