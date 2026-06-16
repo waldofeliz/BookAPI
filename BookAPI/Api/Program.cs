4fa4f2a5e5b7f82398bf49bb3d6cf11445b7f11e
Waldo Feliz <waldofeliz1992@gmail.com>
2026-06-15 20:43:24 -0400

    Documentación del proyecto

--- BookAPI/Api/Program.cs
+++ BookAPI/Api/Program.cs
@@ -9,7 +9,19 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c =>
 {
-    c.SwaggerDoc("v1", new OpenApiInfo { Title = "BookAPI", Version = "v1" });
+    c.SwaggerDoc("v1", 
+        new OpenApiInfo
+        {
+            Title = "BookAPI", 
+            Version = "v1", 
+            Description = "API para la gestión de recursos de biblioteca.",
+            Contact = new OpenApiContact
+                {
+                    Name = "Waldo Feliz", 
+                    Email = "waldofeliz1992@gmail.com"
+                    
+                }
+        });
     c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
     {
         Name = "Authorization",
@@ -36,8 +48,15 @@ builder.Services.AddTransient<ExceptionHandlingMiddleware>();
 
 var app = builder.Build();
 
-app.UseSwagger();
-app.UseSwaggerUI();
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "BookAPI v1");
+        options.RoutePrefix = "swagger";
+    });
+}
 
 app.UseHttpsRedirection();
 
